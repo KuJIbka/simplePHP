f90132fd1e1aa0b847d1ba91999716974dabc649
brin <KuJIbka.polosa@gmail.com>
2017-09-16 22:37:05 +0300

    Fix migration

--- backEnd/src/Main/Migrations/Version20170915125733.php
+++ backEnd/src/Main/Migrations/Version20170915125733.php
@@ -62,7 +62,7 @@ class Version20170915125733 extends AbstractMigration
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE users');
         $this->addSql('DROP TABLE user_limit');
+        $this->addSql('DROP TABLE users');
     }
 }
