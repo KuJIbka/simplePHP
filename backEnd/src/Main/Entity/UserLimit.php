13758dfe03c5e7b026155e45796c54e3d76ff1c7
brin <KuJIbka.polosa@gmail.com>
2017-09-19 14:54:07 +0300

    Remove phpdoc

--- backEnd/src/Main/Entity/UserLimit.php
+++ backEnd/src/Main/Entity/UserLimit.php
@@ -26,51 +26,33 @@ class UserLimit
      */
     private $loginTryCountTime = 0;
 
-    /**
-     * @return User
-     */
     public function getUser(): User
     {
         return $this->user;
     }
 
-    /**
-     * @param User $user
-     */
     public function setUser(User $user): self
     {
         $this->user = $user;
         return $this;
     }
 
-    /**
-     * @return int
-     */
     public function getLoginTryCount(): int
     {
         return $this->loginTryCount;
     }
 
-    /**
-     * @param int $loginTryCount
-     */
     public function setLoginTryCount(int $loginTryCount): self
     {
         $this->loginTryCount = $loginTryCount;
         return $this;
     }
 
-    /**
-     * @return int
-     */
     public function getLoginTryCountTime(): int
     {
         return $this->loginTryCountTime;
     }
 
-    /**
-     * @param int $loginTryCountTime
-     */
     public function setLoginTryCountTime(int $loginTryCountTime): self
     {
         $this->loginTryCountTime = $loginTryCountTime;
